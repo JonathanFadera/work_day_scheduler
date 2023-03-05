98c7b94364d396c09f0e973ce6a06e0747d5bc35
AlbusSeverus <ericsonhnc@gmail.com>
2023-03-04 21:52:38 -0800

    js file commited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,23 @@
 //Variables declaration
 let $currentDayEl = $("#currentDay");
-$currentDayEl.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$currentDayEl.text(moment().format("dddd, MMMM Do YYYY, hh:mm:ss a"));
 let containerEl = $(".container");
 let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 let hoursDisplay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 let currentHour = parseInt(moment().format("h"));
 
-// loop to create the rows and columns for business hours
-for (let i = 0; i < hoursArray.length; i++) {
-  const hour = hoursArray[i];
-  const hourDisplay = hoursDisplay[i];
-  let cssClass = "";
-  if (hour > currentHour) {
-    cssClass = "future";
-  } else if (hour < currentHour) {
-    cssClass = "past";
-  } else {
-    cssClass = "present";
-  }
-
-  // function to update the current day and time
-  function tick() {
-    var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    $currentDayEl.text(now);
-  }
-  setInterval(tick, 1000);
-  
+// loop to create the html for the planner
+  for (let i = 0; i < hoursArray.length; i++) {
+    const hour = hoursArray[i];
+    const hourDisplay = hoursDisplay[i];
+    let cssClass = "";
+    if (hour > currentHour) {
+      cssClass = "future";
+    } else if (hour < currentHour) {
+      cssClass = "past";
+    } else {
+      cssClass = "present";
+    }
   // get the Local Storage if already set
   let getLocalStorage = localStorage.getItem(hour) || "";
   let row = `<div class="row">
@@ -35,6 +27,14 @@ for (let i = 0; i < hoursArray.length; i++) {
   </div>`;
   containerEl.append(row);
 }
+
+  // function to update the current day and time
+  function tick() {
+    var now = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
+    $currentDayEl.text(now);
+  }
+  setInterval(tick, 1000);
+  
 // Event listener onClick to save and set the Local Storage item
 $("button").on("click", (event) => {
   let key = $(event.target).attr("data-hour");
