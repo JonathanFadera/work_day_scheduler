933edc17ac0a5a4ae82565d85f5bbbc959b6e9cf
AlbusSeverus <ericsonhnc@gmail.com>
2023-03-06 21:19:05 -0800

    added ready function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-//Variables declaration
-let $currentDayEl = $("#currentDay");
-$currentDayEl.text(moment().format("dddd, MMMM Do YYYY, hh:mm:ss a"));
-let containerEl = $(".container");
-let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-let hoursDisplay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-let currentHour = parseInt(moment().format("h"));
+$(document).ready(function () {
+  //Variables declaration
+  let $currentDayEl = $("#currentDay");
+  $currentDayEl.text(moment().format("dddd, MMMM Do YYYY, hh:mm:ss a"));
+  let containerEl = $(".container");
+  let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  let hoursDisplay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  let currentHour = parseInt(moment().format("h"));
 
-// loop to create the html for the planner
+  // loop to create the html for the planner
   for (let i = 0; i < hoursArray.length; i++) {
     const hour = hoursArray[i];
     const hourDisplay = hoursDisplay[i];
@@ -18,15 +19,15 @@ let currentHour = parseInt(moment().format("h"));
     } else {
       cssClass = "present";
     }
-  // get the Local Storage if already set
-  let getLocalStorage = localStorage.getItem(hour) || "";
-  let row = `<div class="row">
+    // get the Local Storage if already set
+    let getLocalStorage = localStorage.getItem(hour) || "";
+    let row = `<div class="row">
   <div class="col-2 time-block hour">${hourDisplay}</div>
   <textarea class="col-8 ${cssClass}">${getLocalStorage}</textarea>
   <button class="col-2 saveBtn" data-hour="${hour}">☕</button>
   </div>`;
-  containerEl.append(row);
-}
+    containerEl.append(row);
+  }
 
   // function to update the current day and time
   function tick() {
@@ -34,10 +35,11 @@ let currentHour = parseInt(moment().format("h"));
     $currentDayEl.text(now);
   }
   setInterval(tick, 1000);
-  
-// Event listener onClick to save and set the Local Storage item
-$("button").on("click", (event) => {
-  let key = $(event.target).attr("data-hour");
-  let value = $(event.target).siblings("textarea").val();
-  localStorage.setItem(key, value);
+
+  // Event listener onClick to save and set the Local Storage item
+  $("button").on("click", (event) => {
+    let key = $(event.target).attr("data-hour");
+    let value = $(event.target).siblings("textarea").val();
+    localStorage.setItem(key, value);
+  });
 });
